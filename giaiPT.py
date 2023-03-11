2424867d59e057484f20f036ec818c4f991de48b
Chonn18 <duongvanchon18@gmail.com>
2023-03-11 16:14:11 +0700

    add file giaipt to test git

---
+++ giaiPT.py
@@ -0,0 +1,18 @@
+import math
+
+
+class S:
+    def __init__(self, x):
+        self.x = x
+
+    def calculate(self, n):
+        numerator = (-1)**n * self.x**(2*n)
+        denominator = math.factorial(2*n)
+        return numerator / denominator
+
+
+# tmp = int(input())
+n = int(input())
+s = S(3)  # khởi tạo đối tượng S với x = 3
+for i in range(1, n):
+    print(s.calculate(i))
